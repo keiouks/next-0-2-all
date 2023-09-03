5189a50c5ba4d27930b34d61739a5dc03056380c
liugangyi <keiouks@liugangyideMacBook-Pro.local>
2023-09-03 23:24:08 +0800

    feat(head): css module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -11,6 +12,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
