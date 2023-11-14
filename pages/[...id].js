d26ef71a4fb2dfd2a71bb4aa4083d6bfb3e55ad4
keiouks <13428969714@163.com>
2023-11-14 09:45:41 +0800

    feat(dynamic-path): 新路径产生静态页面成功,接着要处理相对路径引用图片问题,还有列表更新问题和页面更新问题

--- pages/[...id].js
+++ pages/[...id].js
@@ -5,38 +5,39 @@ import Date from '../components/date';
 import { getSortedPostsData, getPostData } from '../lib/posts';
 
 export default function Post({ postData, allPostsData }) {
-    return (
-        <Layout postsData={allPostsData}>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
-              <article>
-              <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-              <div className={utilStyles.lightText}>
-                  <Date dateString={postData.date} />
-              </div>
-              <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-          </article>
-        </Layout>
-      );
+  return (
+    <Layout postsData={allPostsData}>
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+          <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+              <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
-    const paths = getSortedPostsData().map(({id}) => ({params: {id}}));
-    return {
-        paths,
-        fallback: 'blocking',
-    }
+  const paths = getSortedPostsData().map(({id}) => ({params: {id}}));
+  return {
+    paths,
+    fallback: 'blocking',
+  }
 }
 
 export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
-    const allPostsData = getSortedPostsData();
-    return {
-        props: {
-            postData,
-            allPostsData,
-        },
-        revalidate: 3600
-    }
+  const allPostsData = getSortedPostsData();
+  const postData = await getPostData(params.id[0]);
+  console.log(postData);
+  return {
+    props: {
+      postData,
+      allPostsData,
+    },
+    revalidate: 3600
+  }
 }
