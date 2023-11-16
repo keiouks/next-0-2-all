873dcb97629ff8c68d63ce4306b50b3c5e5b0164
keiouks <13428969714@163.com>
2023-11-16 09:53:22 +0800

    feat(nav): 正在处理布局和菜单

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,44 @@ import Link from 'next/link';
 import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { useState } from 'react';
+import styles from '../styles/Home.module.scss';
 import { getSortedPostsData } from '../lib/posts';
 
 
 export default function Home({allPostsData}) {
+  const [showNav, setShowNav] = useState(true);
+  // return (
+  //   <Layout home postsData={allPostsData} >
+  //     <Head>
+  //       <title>{siteTitle}</title>
+  //     </Head>
+  //     <section className={utilStyles.headingMd}>
+  //       <p>this is something good</p>
+  //       <p>
+  //         In this website - you can see the hold world
+  //       </p>
+  //     </section>
+  //   </Layout>
+  // );
   return (
-    <Layout home postsData={allPostsData} >
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>this is something good</p>
-        <p>
-          In this website - you can see the hold world
-        </p>
-      </section>
-    </Layout>
+    <>
+      <div className={{
+        [styles.nav]: true,
+        [styles.hidden]: !showNav,
+      }}>
+        <div className='nav-inner'>
+          <ol className='list'>
+            <li className='item'></li>
+          </ol>
+        </div>
+      </div>
+
+      <div className={{
+        [styles.container]: true,
+        [styles['show-nav']]: showNav,
+      }}></div>
+    </>
   );
 }
 
